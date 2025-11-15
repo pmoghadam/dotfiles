05fdd2172f85366f08ef29876838de8760697ba7
Pejman Moghadam <pmoghadam@yahoo.com>
2025-11-15 21:38:43 +0330

    packages separated from readme

---
+++ docs/install-packages.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+apt-get -y update
+
+apt-get -y install          \
+    arping                  \
+    atop                    \
+    bc                      \
+    bind9-dnsutils          \
+    build-essential         \
+    curl                    \
+    dirmngr                 \
+    dlocate                 \
+    ethtool                 \
+    gdisk                   \
+    htop                    \
+    ifenslave               \
+    iftop                   \
+    iotop                   \
+    iptables                \
+    iptraf-ng               \
+    less                    \
+    links                   \
+    lsb-release             \
+    lsof                    \
+    lvm2                    \
+    dlocate                 \
+    mtr                     \
+    ncdu                    \
+    gdu                     \
+    net-tools               \
+    nload                   \
+    speedometer             \
+    psmisc                  \
+    rsync                   \
+    screen                  \
+    sudo                    \
+    sysstat                 \
+    tcpdump                 \
+    traceroute              \
+    tree                    \
+    vim                     \
+    vlan                    \
+    wget                    \
+    whois                   \
+    xfsprogs                \
+    autoconf                \
+    libtool                 \
+    zip                     \
+    git                     \
+    unzip                   \
+    iptables-persistent     \
+    jq                      \
+    libncurses-dev
+
+apt-get -y install          \
+    i3-wm                   \
+    i3status                \
+    i3lock                  \
+    suckless-tools          \
+    lightdm                 \
+    alacritty               \
+    conky-all               \
+    jcal                    \
+    thunar                  \
+    rofi                    \
+    feh                     \
+    pasystray               \
+    pulseaudio              \
+    scrot                   \
+    avahi-daemon            \
+    avahi-utils             \
+    tmux                    \
+    x11-xserver-utils       \
+    x11-xkb-utils           \
+    firefox-esr
+
