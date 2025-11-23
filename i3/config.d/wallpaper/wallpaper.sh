8677da1c8a8edcd8a76ac8168ec484cf14c8611a
Pejman Moghadam <pmoghadam@yahoo.com>
2025-11-23 15:27:34 +0330

    wallpaper.sh home address edit

--- i3/config.d/wallpaper/wallpaper.sh
+++ i3/config.d/wallpaper/wallpaper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-DIR="~/.config/i3/config.d/wallpaper/wallpapers/"
+DIR="$HOME/.config/i3/config.d/wallpaper/wallpapers/"
 FNAME=$(ls "$DIR" | while read x; do echo "`expr $RANDOM % 1000`:$x"; done | sort -n | sed 's/[0-9]*://' | head -n 1)
 echo "$DIR/$FNAME"
 #DISPLAY=:0 feh --bg-scale "$DIR/$FNAME"
